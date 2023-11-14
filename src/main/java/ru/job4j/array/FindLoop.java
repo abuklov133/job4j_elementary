b9894a4f80a8300c6413ea65553ab6a375de09d2
abuklov133 <abuklov133@gmail.com>
2023-11-14 13:58:34 +0300

    6.5. Поиск индекса в диапазоне. [#156318 #454241]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -1,8 +1,10 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
-        int rst = -1; /* если элемента нет в массиве, то возвращаем -1. */
+        int rst = -1;
         for (int index = 0; index < data.length; index++) {
             if (data[index] == el) {
                 rst = index;
@@ -21,5 +23,24 @@ public class FindLoop {
             }
         }
         return rst;
+    }
+
+    public static void main(String[] args) {
+        int[] data = {9, 2, 1, 5, 7, 0};
+        int min = MinDiapason.findMin(data, 0, data.length - 1);
+        int indexMin = indexInRange(data, min, 0, data.length - 1);
+        System.out.println("Min number in data " + min);
+        System.out.println("Index of min number " + indexMin);
+        SwitchArray.swap(data, indexMin, 3);
+        indexMin = indexInRange(data, min, 0, data.length - 1);
+        System.out.println("New index of min number " + indexMin);
+
+        for (int i = 0; i < data.length; i++) {
+            min = MinDiapason.findMin(data, i, data.length -1);
+            indexMin = indexInRange(data, min, i, data.length - 1);
+            SwitchArray.swap(data, indexMin, i);
+
+        }
+        System.out.println(Arrays.toString(data));
     }
 }
