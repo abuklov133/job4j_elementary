ffe6a1fe671a64c2f06e3904199fd16e4f24389a
abuklov133 <abuklov133@gmail.com>
2024-01-30 06:42:48 +0300

    6.8.1. Жадный алгоритм. Сдача в кофе машине [#34741 #456304]

--- src/test/java/ru/job4j/array/MachineTest.java
+++ src/test/java/ru/job4j/array/MachineTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MachineTest {
+
     @Test
     public void whenMoneyEqualsPrice() {
         int money = 100;
